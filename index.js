07c86c840cb77bad6db96b26292cacb55015ec3d
myildiz17 <muhammetyildiz@gmail.com>
2020-10-26 11:23:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code here!let 
+let main = document.getElementById('main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Muhammet is the champion'
